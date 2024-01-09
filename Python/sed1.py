3565a6fe1e178882c173d907286bc8927dca5e81
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-01-08 23:38:27 -0300

    cami god

--- Python/sed1.py
+++ Python/sed1.py
@@ -1,1 +1,41 @@
-contador_while =
+"""
+Ejercicio 1:
+
+Pedir el nombre y el sueldo, incrementarle un 10% e informar el aumento de su
+sueldo para esa persona.
+"""
+"""
+nombre = input("Ingrese su nombre: ")
+sueldo = float(input("Ingrese su sueldo: "))
+nuevosueldo = sueldo * 0.1 + sueldo
+print(f"Su nuevo sueldo es de {nuevosueldo}, usted es {nombre}")
+"""
+"""
+Ejercicio 2:
+
+Pedir una edad. Informar si la persona es mayor de edad (más de 18 años),
+adolescente (entre 13 y 17 años) o niño (menor a 13 años).
+"""
+"""
+edad = int(input("Ingrese su edad: "))
+if (edad >= 18):
+    print("sos mayor")
+elif (edad >= 13):
+    print("sos adolecente")
+elif (edad >= 1):
+    print("sos niño")
+else:
+    print("No existís")
+"""
+"""
+Ejercicio 3:
+
+Ingresar 5 números enteros, distintos de cero.
+Informar:
+a. Cantidad de pares e impares.
+b. El menor número ingresado.
+c. De los pares el mayor número ingresado.
+d. Suma de los positivos.
+e. Producto de los negativos.
+"""
+
